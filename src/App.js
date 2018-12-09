e7846851d3e08363289e7ebbd011c10f0ae38791
Rodolfo Kuman <rodolfo.kuman@hotmail.com>
2018-12-09 11:55:48 -0500

    formulario

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
-import './App.css';
+import Header from './componentes/Header';
+import AgregarCita from "./componentes/AgregarCita";
+
 
 class App extends Component {
+
+  state = {
+    citas: []
+  }
+
+  crearCita = (nuevaCita) => {
+    const citas = [...this.state.citas, nuevaCita];
+    console.log(citas);
+    this.setState({
+      citas
+    });
+    
+  }
+
   render() {
     return (
-      
+      <div className="container">
+        <Header
+          titulo={'Administrador de Pacientes de Veterinaria'}
+        />
+        <div className="row">
+          <AgregarCita
+            crearCita={this.crearCita}
+          />
+        </div>
+      </div>
     );
   }
 }
